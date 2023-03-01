3d7c773a245ab35b3672f028572959de6ac6ba34
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-03-01 17:05:03 +0100

    yay

--- vue/index.php
+++ vue/index.php
@@ -3,14 +3,13 @@ $page_dir = 'pages';
 $page = $_GET['p'];
 if (isset($page) && $page !== '') {
   if (file_exists($page_dir . '/' . $page . '.php')) {
-    // Page
-    // echo $page_dir . '/' . $page . '.php';
-    // Check if user is autorized to access this page
     $headers = get_headers($page_dir . '/' . $page . '.php');
     $status=substr($headers[0], 9, 3);
     if($status==200) {
+      // Page
       require_once($page_dir . '/' . $page . '.php');
     } else {
+      // 403
       require_once($page_dir . '/403.php');
     }
   } else {
