1039e0c557303ebad7e22c97215e883f1dbed40d
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-03-01 16:42:51 +0100

    Small changes

--- vue/index.php
+++ vue/index.php
@@ -1,26 +1,25 @@
 <?php
-$page_dir = '../vue';
+$page_dir = 'pages';
 $page = $_GET['p'];
 if (isset($page) && $page !== '') {
-    if (file_exists($page_dir . '/' . $page . '.php')) {
-        echo $page_dir . '/' . $page . '.php';
-        // Page
-        // Check if user is autorized to access this page
-        $headers = get_headers($page_dir . '/' . $page . '.php');
-        $status=substr($headers[0], 9, 3);
-        if($status==200){
-            require_once($page_dir . '/' . $page . '.php');
-        } else
-        {
-            require_once($page_dir . '/403.php');
-        }
+  if (file_exists($page_dir . '/' . $page . '.php')) {
+    // Page
+    // echo $page_dir . '/' . $page . '.php';
+    // Check if user is autorized to access this page
+    $headers = get_headers($page_dir . '/' . $page . '.php');
+    $status=substr($headers[0], 9, 3);
+    if($status==200) {
+      require_once($page_dir . '/' . $page . '.php');
     } else {
-        require_once($page_dir . '/404.php');
-        // 404
+      require_once($page_dir . '/403.php');
     }
+  } else {
+    // 404
+    require_once($page_dir . '/404.php');
+  }
 } else {
-    // Home
-    require_once($page_dir . '/home.php');
+  // Home
+  require_once($page_dir . '/home.php');
 }
 
 require_once('layout.php');
---
+++ vue/layout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+    <!-- Chooses the right CSS file depending on the choosen theme
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    -->
+    <link href="assets/css/main.css" rel="stylesheet" />
+    <!-- SVG favicon -->
+    <link rel="icon" href="assets/img/favicon.svg" />
+  </head>
+
+  <body>
+    <header>
+      <?php require_once('pages/includes/header.php'); ?>
+    </header>
+    <main>
+      <?= $content ?>
+    </main>
+    <footer>
+      <?php require_once('pages/includes/footer.php'); ?>
+    </footer>
+  </body>
+</html>
