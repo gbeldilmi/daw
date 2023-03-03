db0bfce2a5f0f7d40296e31bacb860f6e5f4175d
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2023-03-03 16:26:58 +0100

    layout script1

--- controller/login.php
+++ controller/login.php
@@ -1,33 +1,27 @@
 <?php
 require_once '../model/sql-request.php';
 session_start();
-if(Login()==-1){
+if (get_login()) {
     echo "<script>alert('mot passe ou username error');</script>";
-}
-else
-    Redirect('https://example.com/', 200);
-    function Redirect($url, $status)
-    {
-        header('Location: ' . $url, true, $status);
-        exit();
-    }
+} else
+    echo "<script>alert('mot passe ou username good');</script>";
 
-function Login():int{
-$userlogin=$_POST['username'];
-$passlogin=hash('sha256', $_POST['password']);
-//    $userlogin='ciccio';
-//    $passlogin=hash('sha256', 'tooor');
-    $userQuery=array();
-    $userQuery=array_merge($userQuery,getUserLogin());
-    $a=array();
-    for ($i=0;$i<count($userQuery);$i++){
-        $a=array_merge($a,$userQuery[$i]);
-        $id=$a["id"];
-        $username=$a["username"];
-        $password=$a["password"];
-        if($username==$userlogin && $passlogin==$password)
-            return $id;
+
+function get_login(): bool
+{
+    $userlogin = $_POST['username'];
+    $passlogin = hash('sha256', $_POST['password']);
+
+    $userQuery = array();
+    $userQuery = array_merge($userQuery, getUserLogin());
+    $a = array();
+    for ($i = 0; $i < count($userQuery); $i++) {
+        $a = array_merge($a, $userQuery[$i]);
+        $id = $a["id"];
+        $username = $a["username"];
+        $password = $a["password"];
+        if ($username == $userlogin && $passlogin == $password)
+            return true;
     }
-//    print_r($userQuery);
-    return -1;
+    return false;
 }
