94855392b7995c958e455eaf40fa5796d673aeda
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2023-03-08 17:00:37 +0100

    update index

--- vue/index.php
+++ vue/index.php
@@ -1,11 +1,10 @@
 <?php
-$files = glob( __DIR__ . "../controller/*/*.php");
-
+$files = glob(__DIR__ . "../controller/*/*.php");
 foreach ($files as $file) {
-    require($file);   
+  require_once($file);
 }
 $page_dir = '../vue/pages';
-$page = isset($_GET['p']) ? $_GET['p'] : 'home' ;
+$page = isset($_GET['p']) ? $_GET['p'] : 'home';
 if (file_exists($page_dir . '/' . $page . '.php')) {
   // Page
   require_once($page_dir . '/' . $page . '.php');
