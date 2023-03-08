dbf30ba7ce9f1705578c35ea1f3fe09c327b27d0
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2023-03-08 17:09:50 +0100

    color theme update

--- controller/color_theme/get_theme.php
+++ controller/color_theme/get_theme.php
@@ -1,10 +1,12 @@
 <?php
-  // get the color theme used
-  function get_theme():string{
+require_once "../color_theme/toggle_theme.php";
+// get the color theme used
+function get_theme(): string
+{
+  $theme = $_COOKIE['theme'];
+  if (!isset($theme)) {
+    toggle_theme();
     $theme = $_COOKIE['theme'];
-    if (!isset($theme)) {
-      toggle_theme();
-      $theme = $_COOKIE['theme'];
-    }
-    return $theme;
   }
+  return $theme;
+}
