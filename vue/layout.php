0aaea3cd64db877a2e7eeae94c3145c104f3a9e3
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2023-03-08 17:08:57 +0100

    update layout

--- vue/layout.php
+++ vue/layout.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title><?= $title ?></title>
-    <!-- CSS -->
-    <?= '<link href="assets/css/' . get_color_theme() . '.css" rel="stylesheet" />'?>
-    <link href="assets/css/main.css" rel="stylesheet" />
-    <!-- SVG favicon -->
-    <link rel="icon" href="assets/img/favicon.svg" />
-  </head>
+<?php
+  require_once "../controller/color_theme/get_theme.php";
+?>
+<head>
+  <meta charset="utf-8" />
+  <title><?= $title ?></title>
+  <!-- CSS -->
+  <?= '<link href="assets/css/' . get_theme() . '.css" rel="stylesheet" />' ?>
+  <link href="assets/css/main.css" rel="stylesheet" />
+  <!-- SVG favicon -->
+  <link rel="icon" href="assets/img/favicon.svg" />
+</head>
 
-  <body>
-    <header>
-      <?php require_once('pages/includes/header.php'); ?>
-    </header>
-    <main>
-      <?= $content ?>
-    </main>
-    <footer>
-      <?php require_once('pages/includes/footer.php'); ?>
-    </footer>
-  </body>
+<body>
+  <header>
+    <?php require_once('pages/includes/header.php'); ?>
+  </header>
+  <main>
+    <?= $content ?>
+  </main>
+  <footer>
+    <?php require_once('pages/includes/footer.php'); ?>
+  </footer>
+</body>
+
 </html>
