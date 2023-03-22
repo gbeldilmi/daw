4c3fffc955d6ec86c4d00706cf2ff24d7a323ab2
AmenAbouhamou <amen.abouhamou18@gmail.com>
2023-03-22 15:52:21 +0100

    update

--- controller/color_theme/get_theme.php
+++ controller/color_theme/get_theme.php
@@ -1,10 +1,12 @@
 <?php
 // get the color theme used
+
 function get_theme(): string
 {
   $theme = "light";
-  if (!isset($_COOKIE['theme'])) {
+  if (isset($_COOKIE['theme'])) {
     $theme = $_COOKIE['theme'];
-  }
+  }else
+      setcookie('theme', "light", time() + 365*24*3600,"/");
   return $theme;
 }
