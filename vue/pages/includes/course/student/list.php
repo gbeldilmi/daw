e9b622eb6412ef5ada1e396247540148cb9e7e4e
khwissam <126343816+khwissam@users.noreply.github.com>
2023-03-26 01:00:20 +0100

    Update list.php

--- vue/pages/includes/course/student/list.php
+++ vue/pages/includes/course/student/list.php
@@ -1,12 +1,12 @@
 <?php
   $couses = get_followed_courses(); // array of courses
-  ob_start(); ?>
-
-<?php foreach ($couses as $course);
-    echo "<div><h2><?= $course['NAME'] ?></h2>";
-    echo "<p><?= $course['DESCRIPTION'] ?></p>";
-    echo "<a href=course.php?id=<?= $course['id'] ?>Accéder au cours</a> </div>";
-endforeach; ?>
+  ob_start();
+ ?>
+<?php foreach ($couses as $course) {
+    echo "<div><h2>" . $course['NAME'] . "</h2>";
+    echo "<p>" . $course['DESCRIPTION'] . "</p>";
+    echo "<a href='course.php?id=" . $course['id'] . "'>Accéder au cours</a></div>";
+} ?>
 
 <?php
   $content = ob_get_contents();
