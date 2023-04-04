c57fb926ad2cc3c1363bdc77f026e6d66a471e32
nissrinee-12 <126344190+nissrinee-12@users.noreply.github.com>
2023-04-04 16:08:41 +0200

    update

---
+++ courses/get_courses.php
@@ -0,0 +1,10 @@
+<?php
+// get_courses(owner_id)
+// get the courses owned by the user with the given id
+//function get_courses(owner_id):{}
+session_start();
+require_once '../model/sql-request.php';
+function get_courses($owner_id):array{
+    return get_courses($owner_id);
+ 
+} 
