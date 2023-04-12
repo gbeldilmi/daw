7eb1f905772e9687c1d4c8256b2b9463bed1fb00
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-04-12 15:51:23 +0200

    yay

--- vue/pages/includes/test/teacher.php
+++ vue/pages/includes/test/teacher.php
@@ -1,34 +1,29 @@
 <?php
 $test = get_test($_GET['id']); // récupérer le test depuis la base de données
-  $course = get_course($test['course_id']); // récupérer le cours associé au test
-  $user = get_user($_SESSION['user_id']); // récupérer l'utilisateur connecté
+$course = get_course($test['course_id']); // récupérer le cours associé au test
+$user = get_user($_SESSION['user_id']); // récupérer l'utilisateur connecté
+ob_start();
+?>
 
-  // Vérifier si l'utilisateur connecté est le propriétaire du cours
-  if ($course['owner_id'] !== $user['id']) {
-    header("Location: index.php?p=404");
-  }
-  // Vérifier si le test existe
-if (!$test) {
-  header("Location: index.php?p=404");
-}
-  ob_start(); ?>
-
- <h2> Edition du test </h2>
+<h1> Edition du test </h1>
 
 <form action="/../test.php" method="post">
-  <?php foreach ($test['questions'] as $question) { ?>
-    <div>
-      <h3><?php echo $question['question']; ?></h3>
-      <ul>
-        <?php foreach ($question['choices'] as $choice) { ?> <!--pour moi j'ai rajouter un élément à la dtd pour les choix d'une réponse-->
-          <li>
-            <input type="radio" name="answer-<?php echo $question['id']; ?>" id="answer-<?php echo $choice['id']; ?>" value="<?php echo $choice['id']; ?>">
-            <label for="answer-<?php echo $choice['id']; ?>"><?php echo $choice['choice']; ?></label>
-          </li>
-        <?php } ?>
-      </ul>
-    </div>
-  <?php } ?>
+  <input type="hidden" name="test_id" value="<?= $test['id'] ?>">
+  <?php 
+    foreach ($test['questions'] as $question) {
+      echo '<div>';
+      echo '<p>' . $question['question'] . '</p>';
+      echo '<ul>';
+      foreach ($question['choices'] as $choice) {
+        echo '<li>';
+        echo '<input type="checkbox" name="answer-' . $question['id'] . '" id="answer-' . $choice['id'] . '" value="' . $choice['id'] . '">';
+        echo '<label for="answer-' . $choice['id'] . '">' . $choice['choice']. '</label>';
+        echo '</li>';
+      }
+      echo '</ul>';
+      echo '</div>';
+    }
+  ?>
   <button type="submit">Enregistrer</button>
 </form>
 
