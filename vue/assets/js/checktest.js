21cc06f3247120bd6d4e91a1947d0fdadd5c8710
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-04-15 13:31:00 +0200

    student test

---
+++ vue/assets/js/checktest.js
@@ -0,0 +1,29 @@
+function checktest() {
+  let dom_questions = querySelector(".question");
+  let color_true = "var(--color-7)";
+  let color_false = "var(--color-8)";
+  
+  dom_questions.forEach(elmt => {
+    let valid = true;
+    elmt.querySelector(".ansv").forEach(e => {
+      if (!e.checked) {
+        e.parentElement.style.color = color_false;
+        valid = false;
+      }
+    });
+    elmt.querySelector(".ansf").forEach(e => {
+      if (e.checked) {
+        e.parentElement.style.color = color_false;
+        valid = false;
+      }
+    });
+    let ans = elmt.querySelector(".ans")[0];
+    if (valid) {
+      ans.style.color = color_true;
+      ans.innerHTML = "Correct";
+    } else {
+      ans.style.color = color_false;
+      ans.innerHTML = "Incorrect";
+    }
+  });
+}
--- vue/pages/includes/test/student.php
+++ vue/pages/includes/test/student.php
@@ -1,34 +1,34 @@
 <?php
-$test = get_test($_GET['id']); // xml
+$test = get_test($id); // xml
 ob_start();
 ?>
 
 <h1>Test</h1>
 
-<form method='post' action='test.php'>
-  
+<form action="checktest();">
 <?php
-    echo "<input type='hidden' name='test_id' value='" . $id . "'>"; // Soumettre l'ID du test avec le formulaire
-    echo "<ul>";
-    foreach ($test['questions'] as $question) {
-        echo "<li>";
-        echo "<p>" . $question['text'] . "</p>";
-        echo "<ul>";
-        foreach ($question['answers'] as $answer) {
-            echo "<li>";
-            echo "<input type='radio' name='q" . $question['id'] . "' value='" . $answer['id'] . "' required>";
-            echo "<label>" . $answer['text'] . "</label>";
-            echo "</li>";
+    $qcount = 1;
+    foreach ($test->question as $question) {
+        echo '<div class="question" id="q' . $qcount . '">';
+        echo '<p>' . $qcount . '. ' . $question->text . '</p>';
+        echo '<ul>';
+        foreach ($question->answer as $answer) {
+            echo '<li><label><input type="checkbox" ';
+            if ($answer['valid'] == 'true') {
+                echo 'class="ansv"';
+            } else {
+                echo 'class="ansf"';
+            }
+            echo '>' . $answer->text . '</label></li>';
         }
-        echo "</ul>";
-        echo "</li>";
+        echo '</ul><p class="ans"></p></div>';
+        $qcount = $qcount + 1;
     }
 ?>
-
-</ul>
-<button type='submit'>Soumettre</button>
+<input type="submit" value="Valider">
 </form>
 
+<script src="vue/assets/js/checktest.js"></script>
 
 <?php
   $content = ob_get_contents();
