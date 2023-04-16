edfab3d5311b714e61d9b899ae1397237afdfacc
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2023-04-16 13:25:15 +0200

    update home and header

--- vue/pages/includes/header.php
+++ vue/pages/includes/header.php
@@ -1,28 +1,61 @@
-<div>
-<?php //= '<img src="assets/img/' . get_theme() . '.png" width="15" height="15">' ?>
+<style>
 
-  <img src="assets/img/favicon.svg" alt="">
-</div>
+#MonLogo {
+  transform: scale(2);
+  margin-top:39px;
+  margin-left:35px;
+  margin-bottom: -40px;
+}
 
-<div>
+#boutons {
+  float: right;
+}
+
+#boutons ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
+
+#boutons li {
+  margin-left: 1rem;
+}
+
+#header-button{
+  background-color: #FFFFFF;
+  color: black;
+
+  border:none;
+  text-align: center;
+  width:10rem;
+  height:3rem;
+  font-size: 18px;
+  cursor: pointer;
+  border-radius: 1px;
+  font-family: sans-serif;
+}
+
+#header-button:hover {
+  background-color: #FF0000;
+}
+
+
+
+</style>
+
+<div id="MonLogo"><img src="/vue/assets/img/logodark.svg" alt="logo" height= "100px" width="auto" /></div>
+<div id="boutons">
   <nav>
     <ul>
-      <li>
-        <a href="index.php?p=home">Retour à l'accueil</a>
-      </li>
-      <li>
-        <?php
-        require_once "../controller/user/is_connected.php";
-        if (is_connected()) {
-          echo '<a href="index.php?p=logout">Déconnexion</a>';
-        } else {
-          echo '<a href="index.php?p=login">Connexion</a>';
-        }
-        ?>
-      </li>
-    </ul>
-  </nav>
-  <button id="theme" onclick="changeTheme()">
-    <?= '<img src="assets/img/' . get_theme() . '.png" width="15" height="15">' ?>
-  </button>
+
+    <li><button id="header-button">Edudiant</button></li>
+    <li><button id="header-button">Professeur</button></li>
+    <li><button id="header-button">Connexion</button></li>
+</ul>
+
+</nav>
+
 </div>
--- vue/pages/includes/home/nobody.php
+++ vue/pages/includes/home/nobody.php
@@ -1,13 +1,151 @@
 <?php
   ob_start(); ?>
 
-<div>
-  <h1>Bienvenue sur notre site de formation en ligne</h1>
-  <p>Nous sommes une plateforme éducative en ligne qui offre des cours dans différents domaines, tels que la programmation, le marketing, la finance, etc.</p>
-  <p>Nous nous engageons à fournir des cours de qualité, dispensés par des experts dans leur domaine, pour vous aider à acquérir de nouvelles compétences et à améliorer votre carrière professionnelle.</p>
-  <p>Nos cours sont conçus pour être interactifs et adaptés à votre rythme d'apprentissage. Vous pouvez suivre les cours à votre propre rythme et poser des questions à nos instructeurs en ligne si vous avez besoin d'aide.</p>
-  <img src="" alt="Bannière du site" style="width:100%">
+  <style>
+    #red-square {
+      background-color: white;
+      width: 60%;
+      height: 35rem;
+      float: left;
+      top: 0px;
+      word-wrap: break-word;
+      white-space: normal;
+    }
+    #green-square {
+      background-color: white;
+      display:flex;
+      flex-direction: row;
+      margin-top: 23px;
+      width: 23%;
+      height: 33rem;
+      position: relative;
+      top: 15px;
+      left: 0rem;
+    }
+      #image-container {
+
+      position: absolute;
+      top: 65%;
+      left: 63%;
+      transform: translate(-50%, -50%);
+      z-index: 1;
+    }
+
+    #parent {
+      flex-wrap: wrap;
+      position: relative;
+
+    }
+
+    #texte {
+      margin-right:14rem;
+
+    }
+    #get-started-button {
+  background-color: #000000;
+  color: white;
+  border:none;
+  padding: 10px 20px;
+  text-align: center;
+  width:12rem;
+  height:4rem;
+  font-size: 16px;
+  margin: 8rem 5rem;
+  cursor: pointer;
+  border-radius: 1px;
+}
+
+#get-started-button:hover {
+  background-color: #FF0000;
+}
+#txt {
+  text-align: center;
+  font-size: 16px;
+  font-family: sans-serif;
+  color:black;
+}
+#Troiscarre {
+  background-color: white;
+width: 20rem;
+height: 10rem;
+
+}
+#TroiscarreSpecial {
+
+  background-color: white;
+width: 20rem;
+height: 10rem;
+margin-top:5rem;
+}
+#divparent {
+  display:flex;
+  justify-content: space-between;
+}
+#txtDeux {
+  text-align: center;
+  font-size: 25px;
+  font-family: sans-serif;
+  font-weight: bold;
+  color:black;
+}
+#button-basdepage {
+background-color: #000000;
+color: white;
+font-weight: bold;
+border:none;
+text-align: center;
+width:40rem;
+height:5rem;
+font-size: 18px;
+cursor: pointer;
+border-radius: 1px;
+position: relative;
+  left: 50%;
+  transform: translate(-50%, 15%);
+
+}
+
+#button-basdepage:hover {
+background-color: #FF0000;
+}
+
+  </style>
+  <div id="parent">
+  <div id="red-square">
+<h1 id="texte">Bienvenue sur notre site de formation en ligne<br><br></h1>
+<p id="texte">Nous sommes une plateforme éducative en ligne qui offre des cours dans différents domaines, tels que la programmation, le marketing, la finance, etc.<br><br></p>
+<p id="texte">Nous nous engageons à fournir des cours de qualité, dispensés par des experts dans leur domaine, pour vous aider à acquérir de nouvelles compétences et à améliorer votre carrière professionnelle.<br><br></p>
+<p id="texte">Nos cours sont conçus pour être interactifs et adaptés à votre rythme d'apprentissage. Vous pouvez suivre les cours à votre propre rythme et poser des questions à nos instructeurs en ligne si vous avez besoin d'aide.</p>
+<button id="get-started-button">Get Started</button>
+  </div>
+  <div id="green-square"></div>
+    <div id="image-container">
+    <img src="/vue/assets/img/ImgHome.jpg" width="300" height="400" style="border: 1px solid black;" />
+  </div>
+  </div>
+  <br><br><br><br>
+  <div id="section2">
+    <p id="txt"> ProForma te permet de...<br><br></p>
+<div id="divparent">
+<div id="Troiscarre">
+  <p id="txt">Découvrir</p>
+  <p><br><br>les cours qui donneront vie à votre carrière, étudiante.</p></div>
+<div id="TroiscarreSpecial">
+  <p id="txt">T'inserer</p>
+  <p><br><br>dans le monde professionnel en t'offrant des compétences pertinentes et actualisées pour ta futur carriere.</p></div>
+<div id="Troiscarre">
+  <p id="txt">Te motiver</p>
+  <p><br><br>est notre priorité afin que tu puisses atteindre tes objectifs académiques et professionnels avec succès.</p></div>
+
 </div>
+</div>
+
+<div id="section3">
+<p id="txtDeux"> Bienvenue chez ProForma<br></p>
+<button id="button-basdepage">Découvrez nos formations</button>
+
+</div>
+
 
 <?php
   $content = ob_get_contents();
