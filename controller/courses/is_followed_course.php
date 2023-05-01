2934f8e0b46cc893d60d40efff80b29493fc8aed
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-05-01 23:45:03 +0200

    Update is_followed_course.php

--- controller/courses/is_followed_course.php
+++ controller/courses/is_followed_course.php
@@ -3,7 +3,7 @@
   // return true if the user is following the course
   session_start();
 require_once $_SERVER['DOCUMENT_ROOT'].'/model/sql-request.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/controller/user/get_id.php';
   function is_followed_course($id_course):bool{
-      return is_followed_course_model($id_user,$id_course); // must get the id_user from the session
-   
+      return is_followed_course_model(get_id($_SESSION['username']),$id_course);
   } 
