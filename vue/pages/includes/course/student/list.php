6645e4586d56b01606b45a6861d571d0cf1a7b29
DjFeriel <126344022+DjFeriel@users.noreply.github.com>
2023-05-05 01:13:20 +0200

    update

--- vue/pages/includes/course/student/list.php
+++ vue/pages/includes/course/student/list.php
@@ -3,17 +3,27 @@
   $courses = get_followed_courses(); // array of courses
   ob_start(); ?>
 
+
+    
 <?php
+  echo'<header>';
   if (empty($courses)) {
     echo '<h1>Vous ne suivez aucun cours.</h1>';
   } else {
     echo '<h1>Cours suivis :</h1>';
   }
+  echo'</header>';
+  echo'<ul>';
   foreach ($courses as $course) {
-    echo '<div class="course-card"><h2>' . $course['NAME'] . '</h2>';
-    echo '<p>' . $course['DESCRIPTION'] . '</p>';
-    echo '<a href="index.php?p=course&id=' . $course['ID'] . '">Accéder au cours</a> </div>';
-  } ?>
+    echo'<li>';
+    echo '<h2>' . $course['NAME'] . '</h2>';
+    echo '<p><strong>Niveau :</strong> ' . $course['NIVEAU'] . '</p>';
+    echo '<p><strong>Description :</strong> ' . $course['DESCRIPTION'] . '</p>';
+    echo '<a href="index.php?p=course&id=' . $course['ID'] . '">Accéder au cours</a>';
+    echo '</li>';
+  } 
+  echo '</ul>';
+  ?>
 
 <?php
   $content = ob_get_contents();
--- vue/pages/includes/home/student.php
+++ vue/pages/includes/home/student.php
@@ -1,6 +1,30 @@
 <?php
   ob_start(); ?>
 
+
+<style>
+
+
+body {
+  background-color: var(--color-4);
+  color: var(--color-1);
+}
+
+button {
+  background-color: var(--color-3);
+  color: var(--color-4);
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+a {
+  color: var(--color-2);
+  text-decoration: none;
+}
+</style>
+
 <div>
   <a href="index.php?p=course"><button id="show-courses">Afficher mes cours</button></a>
   <a href="index.php?p=forum"><button id="show-courses">Afficher les forums</button></a>
